af4ae66bbc286e2aa23d85600d6022191b79ed79
operator.name <operator.name@protonmail.com>
2019-07-25 05:13:40 +0100

    Fix script to work in sound effects

--- script.js
+++ script.js
@@ -3,17 +3,13 @@ var prevLastTrack = tracks.children[tracks.children.length-1];
 var audioTracks = [];
 
 function scrollToBottom() {
+    prevLastTrack.scrollIntoView();
     let lastTrack = tracks.children[tracks.children.length-1];
     if (lastTrack != prevLastTrack) {
-        prevLastTrack.scrollIntoView();
         prevLastTrack = lastTrack;
     } else {
-        let elAudioLibraryContent = document.querySelector('#audio-library-content');
-        let elAudioLibraryBrowser = elAudioLibraryContent.querySelector('.audio-library-browser');
-        let elAudioLibraryTrackList = elAudioLibraryBrowser.querySelector('.track-list');
-        let elAudioLibraryTrackFooter = elAudioLibraryContent.querySelector('#audio-library-track-footer');
         audioTracks = Array
-            .from(elAudioLibraryTrackList.querySelectorAll('div.audiolibrary-column.audiolibrary-column-download a'))
+            .from(tracks.querySelectorAll('div.audiolibrary-column.audiolibrary-column-download a'))
             .map(function (link) { return link.href; })
             .filter(function (link, i, a) { return i === a.lastIndexOf(link); });
     }
