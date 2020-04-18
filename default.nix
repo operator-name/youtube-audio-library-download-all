1737e9d2fdec23a6f5a5425e78834e7557a84503
operator.name <operator.name@protonmail.com>
2020-04-18 02:17:47 +0100

    Semi working script to get list for aria2c

--- default.nix
+++ default.nix
@@ -4,22 +4,19 @@ let
     writecookies = pkgs.writeShellScriptBin "writecookies" ''
         ${pkgs.xclip}/bin/xclip -selection clipboard -o > cookies.txt
     '';
-    getjson = pkgs.writeShellScriptBin "getjson" ''
-        SI=$((0))
-        echo "$SI"
-
-        # max mr=1000
-        ${pkgs.aria2}/bin/aria2c --out=music0.json --load-cookies=cookies.txt 'https://www.youtube.com/audioswap_ajax?action_get_tracks=1&dl=true&s=music&mr=1000&si=0&qid=0&sh=true'
-    '';
-in stdenv.mkDerivation {    
+in stdenv.mkDerivation {
+    srcs = ./.;
     name = "youtube-audio-library-download-all";
 
     buildInputs = [
         shellcheck
+
         writecookies
-        getjson
+        
+        curl
         aria2 
-        picard 
         jq
+        
+        picard 
     ];
 }
